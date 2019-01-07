0a436b26c211e4fc7d8adc32208bc68ef08325c8
kevinsheep <kevinsheep@vip.qq.com>
2019-01-07 23:14:55 +0800

    via release.js

--- vuepress/docs/.vuepress/config.js
+++ vuepress/docs/.vuepress/config.js
@@ -9,9 +9,14 @@ module.exports = {
         // editLinks: true,
         // editLinkText: '帮助完善此页面',
         nav: [
-            { text: '前端', link: '/' },
-            // { text: '其他', link: '/else/' },
-            { text: '关于', link: '/about.html' }
+            { text: '首页', link: '/' },
+            { text: '前端', link: '/fe' },
+            { text: '杂谈', link: '/else/' },
+            { text: '关于', link: '/about' }
+        ],
+        sidebar: [
+            '/',
+            ['/about', '关于']
         ]
     },
     dest: '../docs/'
