99a61f891fb2b1c3ff9eaf26fcf72dd840ae34de
kevinsheep <kevinsheep@vip.qq.com>
2021-07-11 11:00:09 +0800

    feat: calc style

--- docs/calc/dist/calc.js
+++ docs/calc/dist/calc.js
@@ -1,50 +1,57 @@
-var MAX = 100;
+const MAX = 100;
+const MIN = 6;
+const AMOUNT = 12;
 function getNumber(max) {
     return Math.ceil(Math.random() * max);
 }
 function getPlus() {
-    var r1 = getNumber(MAX);
-    var r2 = getNumber(MAX - r1);
-    var res = r1 + r2;
-    return { r1: r1, r2: r2, res: res, operator: '+' };
+    const r1 = getNumber(MAX - MIN * 2) + MIN; // 避免出现太简单的
+    const r2 = getNumber(MAX - r1 - MIN * 2) + MIN;
+    const res = r1 + r2;
+    console.log({ r1, r2, res });
+    return { r1, r2, res, operator: '+' };
 }
 function getMinus() {
-    var _a = getPlus(), p1 = _a.r1, p2 = _a.r2, res = _a.res;
+    const { r1: p1, r2: p2, res } = getPlus();
     return { r1: res, r2: p2, res: p1, operator: '-' };
 }
 function getFList() {
-    var list = [];
-    for (var i = 0; i < 8; ++i) {
-        var d = Math.random();
-        var f = d > .5
+    const list = [];
+    for (let i = 0; i < AMOUNT; ++i) {
+        const d = Math.random();
+        const f = d > .5
             ? getPlus()
             : getMinus();
         list.push(f);
     }
     return list;
 }
-function formatList(list, isShowResult) {
-    if (isShowResult === void 0) { isShowResult = false; }
-    var str = [];
-    str = list.map(function (f) { return (f.r1 + " " + f.operator + " " + f.r2 + " = " + (isShowResult ? f.res : '')); });
-    return str.join('<br />');
+function formatDigi(num) {
+    return num.toString().padStart(2, ' ');
+}
+function formatList(list, isShowResult = false) {
+    let str = [];
+    str = list.map(f => (`<li>${formatDigi(f.r1)} ${f.operator} ${formatDigi(f.r2)} = ${isShowResult ? f.res : ''}</li>`));
+    return `<ul>
+        ${str.join('')}
+        </ul>`;
 }
 (function () {
-    var list = document.createElement('div');
-    var btn = document.createElement('button');
-    var btn2 = document.createElement('button');
+    const list = document.createElement('div');
+    const btn = document.createElement('button');
+    const btn2 = document.createElement('button');
     list.className = 'calcBox';
     btn.innerText = '显示结果';
     btn2.innerText = '换一组';
     document.body.appendChild(list);
     document.body.appendChild(btn);
     document.body.appendChild(btn2);
-    var flist = getFList();
+    let flist = getFList();
     list.innerHTML = formatList(flist);
-    btn.onclick = function () {
+    btn.onclick = () => {
         list.innerHTML = formatList(flist, true);
     };
-    btn2.onclick = function () {
+    btn2.onclick = () => {
         flist = getFList();
         list.innerHTML = formatList(flist);
     };
