5894ea3c82c721ea231458b9adb8ccb19ef3fd13
kevinsheep <kevinsheep@vip.qq.com>
2023-05-18 23:15:07 +0800

    style: use base64 logo

--- docs/.vitepress/components/Avatar.vue
+++ docs/.vitepress/components/Avatar.vue
@@ -1,7 +1,11 @@
+<script setup>
+import { avatar, logo_text } from '../base-img.js';
+</script>
+
 <template>
     <header class="hero">
-        <img src="/assets/img/avatar.png" />
+        <img :src="avatar" />
         <p>JUST GO BEYOND YOUR</p>
-        <img src="/assets/img/logo-text.png" />
+        <img :src="logo_text" />
     </header>
 </template>
