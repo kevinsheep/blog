d8dab842ce56acbd45c23a9dc131b4ad42f17341
kevinsheep <xkevinsheep@gmail.com>
2024-06-10 22:18:29 +0800

    style: scroller and toggle btn

--- docs/.vitepress/components/CollapseMenu.vue
+++ docs/.vitepress/components/CollapseMenu.vue
@@ -16,32 +16,38 @@ const toggleMenu = () => {
 
 <style scoped lang="stylus">
 .toggle-btn {
-    position fixed
-    z-index 80
-    top 250px
-    left 50%
-    margin-left -465px
-    display inline-block
-    font-weight bolder
-    font-size 21px
-    cursor pointer
-    width 32px
-    height 32px
-    line-height 32px
-    text-align center
-    border-radius 16px
-    font-family monospace
-    color var(--vp-button-brand-text)
-    background-color var(--vp-c-brand)
-    box-shadow 0 0 10px rgba(0, 0, 0, 0.3)
-    transition all 0.5s
+    display none
 }
-.collasped {
+.has-sidebar {
     .toggle-btn {
-        left -5px
-        margin-left 0
+        position fixed
+        z-index 80
+        top 250px
+        left 50%
+        margin-left -465px
+        display inline-block
+        font-weight bolder
+        font-size 21px
+        cursor pointer
+        width 32px
+        height 32px
+        line-height 32px
+        text-align center
+        border-radius 16px
+        font-family monospace
+        color var(--vp-button-brand-text)
+        background-color var(--vp-c-brand)
+        box-shadow 0 0 10px rgba(0, 0, 0, 0.3)
+        transition all 0.5s
+    }
+    .collasped {
+        .toggle-btn {
+            left -5px
+            margin-left 0
+        }
     }
 }
+
 @media (max-width: 1440px) {
     .toggle-btn {
         left 260px
